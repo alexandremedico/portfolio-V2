ce19028aea0286d096e92e85c8890d2a80d6ea0c
Medico Alexandre <alexandre.medico@hotmail.fr>
2020-05-13 15:20:22 +0200

    Ajout JS + petite modif HTML et CSS

--- js/script.js
+++ js/script.js
@@ -1,28 +1,22 @@
-// window.onscroll = function() {myFunction()};
+window.onscroll = function() {myFunction()};
 
-// function myFunction() {
-// if (document.body.scrollTop > 100) {
-//     window.document.getElementById("declencheur1").className = "part1 animated2 fadeInLeft";
-//     window.document.getElementById("declencheur2").className = "part1 animated2 fadeInLeft";
-//     window.document.getElementById("declencheur3").className = "part1 animated2 fadeInLeft";
-//     window.document.getElementById("declencheur4").className = "part1 animated2 fadeInLeft";
-//     window.document.getElementById("declencheur5").className = "part1 animated2 fadeInLeft";
-//     window.document.getElementById("declencheur6").className = "part1 animated2 fadeInLeft";
-//     window.document.getElementById("declencheur7").className = "part2 animated3 fadeInRight";
-//   } else {
-//     window.document.getElementById("declencheur1").className = "";
-//     window.document.getElementById("declencheur2").className = "";
-//     window.document.getElementById("declencheur3").className = "";
-//     window.document.getElementById("declencheur4").className = "";
-//     window.document.getElementById("declencheur5").className = "";
-//     window.document.getElementById("declencheur6").className = "";
-//     window.document.getElementById("declencheur7").className = "";
-//   }
-// }
+function myFunction() {
+  if (document.body.scrollTop > 450 || document.documentElement.scrollTop > 450) {
+      window.document.getElementById('declencheur1').classList.remove('none');
+      window.document.getElementById('declencheur1').classList.add('animated2','fadeInLeft');
+      window.document.getElementById('declencheur2').classList.remove('none');
+      window.document.getElementById('declencheur2').classList.add("animated3","fadeInRight");
+    }
 
-// document.getElementById("Moi").addEventListener("scroll", myFunction);
-
-// function myFunction() {
-//   window.document.getElementById("declencheur1").className = "part1 animated2 fadeInLeft";
-//   window.document.getElementById("declencheur2").className = "part2 animated3 fadeInRight";
-// }
+  if (document.body.scrollTop > 1350 || document.documentElement.scrollTop > 1350) {
+      window.document.getElementById('hardSkill').classList.remove('none');
+      window.document.getElementById('hardSkill').classList.add('animation');
+      window.document.getElementById('html').classList.add('animation');
+      window.document.getElementById('css').classList.add('animation');
+      window.document.getElementById('javascript').classList.add('animation');
+      window.document.getElementById('nodeJS').classList.add('animation');
+      window.document.getElementById('angularJS').classList.add('animation');
+      window.document.getElementById('softSkill').classList.remove('none');
+      window.document.getElementById('softSkill').classList.add('fadeInUp','animated','animatedFadeInUp');
+  }
+}
