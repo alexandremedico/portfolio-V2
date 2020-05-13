2b5610f7f45942bc7aa8ff7888841123d69b5d4e
Medico Alexandre <alexandre.medico@hotmail.fr>
2020-05-13 14:28:04 +0200

    first version

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// window.onscroll = function() {myFunction()};
+
+// function myFunction() {
+// if (document.body.scrollTop > 100) {
+//     window.document.getElementById("declencheur1").className = "part1 animated2 fadeInLeft";
+//     window.document.getElementById("declencheur2").className = "part1 animated2 fadeInLeft";
+//     window.document.getElementById("declencheur3").className = "part1 animated2 fadeInLeft";
+//     window.document.getElementById("declencheur4").className = "part1 animated2 fadeInLeft";
+//     window.document.getElementById("declencheur5").className = "part1 animated2 fadeInLeft";
+//     window.document.getElementById("declencheur6").className = "part1 animated2 fadeInLeft";
+//     window.document.getElementById("declencheur7").className = "part2 animated3 fadeInRight";
+//   } else {
+//     window.document.getElementById("declencheur1").className = "";
+//     window.document.getElementById("declencheur2").className = "";
+//     window.document.getElementById("declencheur3").className = "";
+//     window.document.getElementById("declencheur4").className = "";
+//     window.document.getElementById("declencheur5").className = "";
+//     window.document.getElementById("declencheur6").className = "";
+//     window.document.getElementById("declencheur7").className = "";
+//   }
+// }
+
+// document.getElementById("Moi").addEventListener("scroll", myFunction);
+
+// function myFunction() {
+//   window.document.getElementById("declencheur1").className = "part1 animated2 fadeInLeft";
+//   window.document.getElementById("declencheur2").className = "part2 animated3 fadeInRight";
+// }
