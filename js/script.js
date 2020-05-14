0f082669cd2a37a5a8a07c93e5ca5d1dfbb94a51
Medico Alexandre <alexandre.medico@hotmail.fr>
2020-05-14 15:40:39 +0200

    Modif JS pour responsive mobile

--- js/script.js
+++ js/script.js
@@ -2,25 +2,25 @@ window.onscroll = function() {myFunction()};
 
 function myFunction() {
     if (screen.width <= 425) {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        // if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
             console.log('telephone')
-            window.document.getElementById('declencheur1').classList.remove('none');
-            window.document.getElementById('declencheur1').classList.add('animated2','fadeInLeft');
-            window.document.getElementById('declencheur2').classList.remove('none');
-            window.document.getElementById('declencheur2').classList.add("animated3","fadeInRight");
-          }
+        //     window.document.getElementById('declencheur1').classList.remove('none');
+        //     window.document.getElementById('declencheur1').classList.add('animated2','fadeInLeft');
+        //     window.document.getElementById('declencheur2').classList.remove('none');
+        //     window.document.getElementById('declencheur2').classList.add("animated3","fadeInRight");
+        //   }
       
-        if (document.body.scrollTop > 850 || document.documentElement.scrollTop > 850) {
-            window.document.getElementById('hardSkill').classList.remove('none');
-            window.document.getElementById('hardSkill').classList.add('animation');
-            window.document.getElementById('html').classList.add('animation');
-            window.document.getElementById('css').classList.add('animation');
-            window.document.getElementById('javascript').classList.add('animation');
-            window.document.getElementById('nodeJS').classList.add('animation');
-            window.document.getElementById('angularJS').classList.add('animation');
-            window.document.getElementById('softSkill').classList.remove('none');
-            window.document.getElementById('softSkill').classList.add('fadeInUp','animated','animatedFadeInUp');
-        }
+        // if (document.body.scrollTop > 850 || document.documentElement.scrollTop > 850) {
+        //     window.document.getElementById('hardSkill').classList.remove('none');
+        //     window.document.getElementById('hardSkill').classList.add('animation');
+        //     window.document.getElementById('html').classList.add('animation');
+        //     window.document.getElementById('css').classList.add('animation');
+        //     window.document.getElementById('javascript').classList.add('animation');
+        //     window.document.getElementById('nodeJS').classList.add('animation');
+        //     window.document.getElementById('angularJS').classList.add('animation');
+        //     window.document.getElementById('softSkill').classList.remove('none');
+        //     window.document.getElementById('softSkill').classList.add('fadeInUp','animated','animatedFadeInUp');
+        // }
     } else if (screen.width <= 800 && screen.width > 425) {
         if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
             console.log('tablette petite')
