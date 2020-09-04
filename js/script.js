2237eb748051ce6e4b43b2babe9b15a1b0b19747
Medico Alexandre <alexandre.medico@hotmail.fr>
2020-09-04 14:46:33 +0200

    ajout function age temps reel

--- js/script.js
+++ js/script.js
@@ -397,3 +397,15 @@ if (innerWidth <= 425) {
 if (innerWidth > 425) {
     document.getElementById('liensMobile').remove();
 }
+
+function age() {
+    let myAge = 598057200000;
+    let time = Date.now();
+    let resultAge = time - myAge;
+    resultAge = resultAge / 86400;
+    resultAge = resultAge / 365000;
+    resultAge = Math.floor(resultAge);
+    document.getElementById('age').innerHTML = resultAge;
+}
+
+age();
